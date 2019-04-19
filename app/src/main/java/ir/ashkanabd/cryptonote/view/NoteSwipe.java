f5535ae14d16556f3fcdb803a65369bd3d249398
AshkanAbd <kotksthegame@gmail.com>
2019-04-19 06:55:40 +0430

    Create a delete dialog for notes when deleting.

--- app/src/main/java/ir/ashkanabd/cryptonote/view/NoteSwipe.java
+++ app/src/main/java/ir/ashkanabd/cryptonote/view/NoteSwipe.java
@@ -1,20 +1,50 @@
 package ir.ashkanabd.cryptonote.view;
 
+import android.widget.TextView;
+
+import com.afollestad.materialdialogs.MaterialDialog;
+
 import co.dift.ui.SwipeToAction;
+import ir.ashkanabd.cryptonote.R;
 import ir.ashkanabd.cryptonote.StartActivity;
 import ir.ashkanabd.cryptonote.note.Note;
 
 public class NoteSwipe implements SwipeToAction.SwipeListener<Note> {
     private StartActivity activity;
+    private MaterialDialog deleteDialog;
+    private TextView deleteText;
+    private TextView deleteButton;
+    private TextView cancelButtonDeleteDialog;
+    private Note currentItem;
 
     public NoteSwipe(StartActivity activity) {
         this.activity = activity;
+        setupDeleteDialog();
     }
+
+    private void setupDeleteDialog() {
+        deleteDialog = new MaterialDialog(activity);
+        deleteDialog.setCancelable(false);
+        deleteDialog.setContentView(R.layout.note_delete_layout);
+        deleteText = deleteDialog.findViewById(R.id.delete_dialog_text);
+        deleteButton = deleteDialog.findViewById(R.id.delete_dialog_delete_btn);
+        cancelButtonDeleteDialog = deleteDialog.findViewById(R.id.delete_dialog_cancel_btn);
+        deleteButton.setOnClickListener((v) -> deleteNote());
+        cancelButtonDeleteDialog.setOnClickListener((v) -> deleteDialog.dismiss());
+    }
+
+    private void deleteNote() {
+        activity.getNoteHandler().deleteNote(currentItem);
+        activity.refreshNotes();
+        deleteDialog.dismiss();
+    }
+
 
     @Override
     public boolean swipeLeft(Note itemData) {
-        activity.getNoteHandler().deleteNote(itemData);
-        activity.refreshNotes();
+        currentItem = itemData;
+        deleteText.setText("Delete note " + itemData.getTitle() + "?");
+        deleteDialog.show();
         return true;
     }
 
